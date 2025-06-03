a29facf5586023ddc14018d616ec545881691c53
grace <896922176@qq.com>
2025-06-03 23:47:08 +0800

    Text test

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,14 +43,14 @@ android {
         }
     }
 
-    signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
-        }
-    }
+//    signingConfigs {
+//        create("release") {
+//            keyAlias = keystoreProperties["keyAlias"] as String
+//            keyPassword = keystoreProperties["keyPassword"] as String
+//            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+//            storePassword = keystoreProperties["storePassword"] as String
+//        }
+//    }
     buildTypes {
         release {
 //            isMinifyEnabled = true
@@ -61,7 +61,7 @@ android {
 //                    "proguard-rules.pro"
 //                )
 //            )
-            signingConfig = signingConfigs.getByName("release")
+//            signingConfig = signingConfigs.getByName("release")
         }
 
         getByName("debug") {
